bea645b44d6d0dd9a64d4f839451bee7834f5689
Jesus Arias <jesus.d.arias96@gmail.com>
2017-08-20 20:10:39 -0600

    db

--- routes/articulos.js
+++ routes/articulos.js
@@ -6,7 +6,7 @@ var db = new sqlite3.Database('sistema_pedido.db');
 
 router.get('/', function(request, response) {
     db.all("SELECT * FROM tbl_articulo", function(err, rows) {
-      console.log("GET request for articulos");
+      console.log(row);
       response.render('articulos', {
         title: 'Articulos',
         rows: rows

b00f3f8162436daea7fd0c74dccfc1c3a44fbf6e
Jesus <jesus.d.arias96@gmail.com>
2017-12-12 01:16:45 -0600

    Commit 1

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//MySQL
+const mysql = require('mysql');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
